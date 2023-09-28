be458249fe95464f83d70d0dbc1c73f8fecddf4f
Joelma Giovanna <142359801+JGiotech@users.noreply.github.com>
2023-09-28 14:04:53 -0300

    Questão 2

---
+++ Questão_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    
+    int numero;
+    int soma = 0;
+
+    scanf("%d", &numero);
+
+    if (numero % 2 == 0) {
+        printf("%d eh par\n", numero);
+    } else {
+        printf("%d eh impar\n", numero);
+    }
+
+    int i = numero;
+    for (; i != 0; i /= 10) {
+        soma += i % 10;
+    }
+
+    printf("A soma dos algarismos de %d eh %d", numero, soma);
+
+    return 0;
+}
